135b28291983c36055445a1bbf6a99fbae9a02aa
wk1995 <1226426603@qq.com>
2020-08-23 22:21:06 +0800

    添加test项目

---
+++ test/src/main/java/com/wk/test/TestMainActivity.kt
@@ -0,0 +1,12 @@
+package com.wk.test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test_main_activity)
+    }
+}
