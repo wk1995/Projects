f713305efae11885e73a8f51c76d6eb16d61dd1c
wk1995 <1226426603@qq.com>
2020-11-16 07:29:15 +0800

    android studio 3.6打不开 profiler 换4.1

--- test/src/main/java/com/wk/test/TestMainActivity.kt
+++ test/src/main/java/com/wk/test/TestMainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import com.wk.test.aidl.AIDLActivity
+import com.wk.test.bitmap.TestBitmapActivity
 import com.wk.test.touch.TestMotionEventActivity
 import kotlinx.android.synthetic.main.test_main_activity.*
 
@@ -12,17 +13,19 @@ class TestMainActivity : BaseTestActivity(), TestListAdapter.ITestItemClickListe
         const val TOUCH_EVENT = "事件分发"
         const val AIDL = "AIDL"
         const val HANDLER = "HANDLER"
+        const val BITMAP="BITMAP"
     }
 
     private val list by lazy {
-        listOf(TOUCH_EVENT, AIDL,HANDLER)
+        listOf(TOUCH_EVENT, AIDL,HANDLER,BITMAP)
     }
 
     private val map by lazy {
         mapOf(
                 Pair(TOUCH_EVENT, TestMotionEventActivity::class.java),
                 Pair(AIDL, AIDLActivity::class.java),
-                Pair(HANDLER, TestMotionEventActivity::class.java)
+                Pair(HANDLER, TestMotionEventActivity::class.java),
+                Pair(BITMAP, TestBitmapActivity::class.java)
         )
     }
 
