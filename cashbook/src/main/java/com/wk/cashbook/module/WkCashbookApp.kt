a131bee3bb2960298237f80dddc53967a5128213
wangkang <1226426603@qq.com>
2021-01-25 14:46:52 +0800

    cashbook 记账本init

---
+++ cashbook/src/main/java/com/wk/cashbook/module/WkCashbookApp.kt
@@ -0,0 +1,13 @@
+package com.wk.cashbook.module
+
+import com.wk.projects.common.BaseApplication
+
+/**
+ * @author      :wangkang_shenlong
+ * email        :shenlong.wang@tuya.com
+ * create date  : 2021/01/25
+ * desc         :
+ */
+
+
+class WkCashbookApp :BaseApplication()
