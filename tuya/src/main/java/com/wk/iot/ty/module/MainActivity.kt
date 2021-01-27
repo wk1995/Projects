42fe52b204bb92a6a50adf07b4606f4bab3f46af
wangkang <1226426603@qq.com>
2021-01-27 19:10:40 +0800

    新增涂鸦sdk module init

---
+++ tuya/src/main/java/com/wk/iot/ty/module/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wk.iot.ty.module
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.wk.iot.ty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ tuya/src/main/java/com/wk/iot/ty/module/TuyaApp.kt
@@ -0,0 +1,13 @@
+package com.wk.iot.ty.module
+
+import com.wk.projects.common.BaseApplication
+
+/**
+ * @author      :wangkang_shenlong
+ * email        :shenlong.wang@tuya.com
+ * create date  : 2021/01/27
+ * desc         :
+ */
+
+
+class TuyaApp:BaseApplication ()
