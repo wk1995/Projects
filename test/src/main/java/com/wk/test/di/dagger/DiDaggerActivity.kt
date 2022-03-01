4bb6b6a570c8fb07973bd8aceeab778094877b6e
wangkang <shenlong.wang@tuya.com>
2022-03-01 15:14:57 +0800

    依赖反转 dagger 和 hilt 页面

---
+++ test/src/main/java/com/wk/test/di/dagger/DiDaggerActivity.kt
@@ -0,0 +1,13 @@
+package com.wk.test.di.dagger
+
+import com.wk.test.BaseTestActivity
+import com.wk.test.R
+
+class DiDaggerActivity : BaseTestActivity() {
+
+    override fun initLayout()= R.layout.test_di_dagger_activity
+
+    override fun initView() {
+
+    }
+}
---
+++ test/src/main/java/com/wk/test/di/hilt/DiHiltActivity.kt
@@ -0,0 +1,13 @@
+package com.wk.test.di.hilt
+
+import com.wk.test.BaseTestActivity
+import com.wk.test.R
+
+class DiHiltActivity: BaseTestActivity() {
+
+    override fun initLayout()= R.layout.test_di_hilt_activity
+
+    override fun initView() {
+
+    }
+}
