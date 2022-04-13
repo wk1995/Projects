8365b17bdf1afe8454535a948561c17383b910ad
wangkang <shenlong.wang@tuya.com>
2022-04-13 17:23:40 +0800

    kotlin 协程相关

--- test/src/main/java/com/wk/test/coroutines/CoroutinesMainActivity.kt
+++ test/src/main/java/com/wk/test/coroutines/CoroutinesMainActivity.kt
@@ -1,12 +1,29 @@
 package com.wk.test.coroutines
 
+import android.content.Intent
+import com.wk.test.BaseListActivity
 import com.wk.test.BaseTestActivity
 import com.wk.test.R
 
-class CoroutinesMainActivity : BaseTestActivity() {
+class CoroutinesMainActivity : BaseListActivity<Unit>() {
+    companion object {
+        const val SUSPEND = "suspend"
+        const val FLOW="flow"
+        const val FLOW_AND_STATE_FLOW="flow And StateFlow"
+        const val FLOW_AND_SHARE_FLOW="flow And ShareFlow"
+        const val STATE_FLOW_AND_SHARE_FLOW="StateFlow And ShareFlow"
+    }
 
-    override fun initLayout()= R.layout.test_coroutines_main_activity
+    override fun getListMap(): Map<String, Unit> {
+        return mapOf(
+            Pair(SUSPEND, startActivity(CoroutinesSuspendActivity::class.java)),
+            Pair(FLOW, startActivity(CoroutinesMainActivity::class.java)),
+            Pair(FLOW_AND_SHARE_FLOW, startActivity(CoroutinesMainActivity::class.java)),
+            Pair(STATE_FLOW_AND_SHARE_FLOW, startActivity(CoroutinesMainActivity::class.java))
+        )
+    }
 
-    override fun initView() {
+    private fun startActivity(cls: Class<*>) {
+        startActivity(Intent(this, cls))
     }
 }
---
+++ test/src/main/java/com/wk/test/coroutines/suspend/JavaMethod.java
@@ -0,0 +1,143 @@
+package com.wk.test.coroutines.suspend;
+
+import com.wk.test.TestLogUtil;
+
+import org.jetbrains.annotations.NotNull;
+
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+import kotlin.coroutines.CoroutineContext;
+import kotlin.coroutines.EmptyCoroutineContext;
+import kotlin.coroutines.intrinsics.IntrinsicsKt;
+
+/**
+ * @author :wangkang_shenlong
+ * email        :shenlong.wang@tuya.com
+ * create date  : 2022/04/12
+ * desc         :
+ */
+
+
+public class JavaMethod {
+
+    /**一个回调的java 方法*/
+    interface IJavaInterfaceOneMethod {
+        void oneMethod(Object object);
+    }
+
+    /**两个个回调的java 方法*/
+    interface IJavaInterfaceTwoMethod {
+        void oneMethod(Object object);
+
+        void twoMethod(Object object);
+    }
+
+    /**多个回调的java 方法*/
+    interface IJavaInterfaceThreeMethod {
+
+        void oneMethod(Object object);
+
+        void twoMethod(Object object);
+
+        void threeMethod(Object object);
+    }
+
+    /**
+     * 空方法
+     * */
+    public void javaEmptyMethod() {
+        TestLogUtil.log("javaSimpleMethod");
+    }
+
+    /**
+     * 使用一个Java回调的方法
+     * */
+    public void javaSimpleCallBackMethod(IJavaInterfaceOneMethod callback) {
+        TestLogUtil.log("javaSimpleCallBackMethod OneMethod");
+        callback.oneMethod("IJavaInterfaceOneMethod");
+    }
+
+    /**
+     * 使用两个Java回调的方法
+     * */
+    public void javaSimpleCallBackMethod(IJavaInterfaceTwoMethod callback) {
+        TestLogUtil.log("javaSimpleCallBackMethod TwoMethod");
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(1000);
+                if (i == 4) {
+                    callback.oneMethod("IJavaInterfaceTwoMethod oneMethod");
+                }
+            }
+        } catch (Exception e) {
+            callback.twoMethod("IJavaInterfaceTwoMethod  oneMethod");
+        }
+
+    }
+
+    /**
+     * 使用多个Java回调的方法
+     * */
+    public void javaSimpleCallBackMethod(IJavaInterfaceThreeMethod callback) {
+        TestLogUtil.log("javaSimpleCallBackMethod ThreeMethod");
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(1000);
+                if (i == 4) {
+                    callback.oneMethod("IJavaInterfaceThreeMethod oneMethod");
+                }
+            }
+        } catch (Exception e) {
+            callback.twoMethod("IJavaInterfaceThreeMethod twoMethod");
+        }
+        callback.threeMethod("IJavaInterfaceThreeMethod  oneMethod");
+    }
+
+
+    /**
+     * Java使用 suspend方法
+     * */
+    public void invokeSuspendMethod(SuspendKotlinMethod suspendKotlinMethod) {
+        Object value = suspendKotlinMethod.returnSuspend(new Continuation<Integer>() {
+            @NotNull
+            @Override
+            public CoroutineContext getContext() {
+                return EmptyCoroutineContext.INSTANCE;
+            }
+
+            @Override
+            public void resumeWith(@NotNull Object o) {
+                TestLogUtil.log("o:  " + o.getClass().getSimpleName());
+                if (o instanceof Integer) {
+                    handleResult(o);
+                } else {
+                    Throwable throwable = (Throwable) o;
+                    throwable.printStackTrace();
+                }
+            }
+        });
+
+        if (value == IntrinsicsKt.getCOROUTINE_SUSPENDED()) {
+            TestLogUtil.log("Suspended.");
+        } else {
+            handleResult(value);
+        }
+    }
+
+    private void handleResult(Object o) {
+        TestLogUtil.log("The result is " + o);
+    }
+
+
+    public void execute() {
+        CallBackContinuation callBackContinuation = new CallBackContinuation();
+        JavaContinuation javaContinuation = new JavaContinuation(callBackContinuation);
+        try {
+            callBackContinuation.await();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        javaContinuation.resumeWith(Unit.INSTANCE);
+    }
+
+}
