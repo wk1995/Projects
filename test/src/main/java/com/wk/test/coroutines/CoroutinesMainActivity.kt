9b3efe34991c5e2aa7442f196b45a4221c564577
wk <1226426603@qq.com>
2022-04-18 07:44:20 +0800

    优化协程flow相关页面以及stateFlow用法

--- test/src/main/java/com/wk/test/coroutines/CoroutinesMainActivity.kt
+++ test/src/main/java/com/wk/test/coroutines/CoroutinesMainActivity.kt
@@ -2,23 +2,23 @@ package com.wk.test.coroutines
 
 import com.wk.test.BaseListActivity
 import com.wk.test.coroutines.flow.CoroutinesFlowActivity
+import com.wk.test.coroutines.flow.CoroutinesStateFlowActivity
 import com.wk.test.coroutines.suspend.CoroutinesSuspendActivity
 
 class CoroutinesMainActivity : BaseListActivity<Unit>() {
     companion object {
         const val SUSPEND = "suspend"
         const val FLOW="flow"
-        const val FLOW_AND_STATE_FLOW="flow And StateFlow"
-        const val FLOW_AND_SHARE_FLOW="flow And ShareFlow"
-        const val STATE_FLOW_AND_SHARE_FLOW="StateFlow And ShareFlow"
+        const val STATE_FLOW="StateFlow"
+        const val SHARE_FLOW="ShareFlow"
     }
 
     override fun getListMap(): Map<String, Unit> {
         return mapOf(
             Pair(SUSPEND, startActivity(CoroutinesSuspendActivity::class.java)),
             Pair(FLOW, startActivity(CoroutinesFlowActivity::class.java)),
-            Pair(FLOW_AND_SHARE_FLOW, startActivity(CoroutinesMainActivity::class.java)),
-            Pair(STATE_FLOW_AND_SHARE_FLOW, startActivity(CoroutinesMainActivity::class.java))
+            Pair(STATE_FLOW, startActivity(CoroutinesStateFlowActivity::class.java)),
+            Pair(SHARE_FLOW, startActivity(CoroutinesStateFlowActivity::class.java))
         )
     }
 }
